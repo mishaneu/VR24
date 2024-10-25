d501ac42919f3e3df070c4058a99efb782363d0b
Mishanyazzz <doctorfomincov2005@mail.ru>
2024-10-25 15:18:47 +0300

    Task №1 (ClickBut)
    Задание №1 по VR

--- VR24/Assets/Scripts/VRtest.cs
+++ VR24/Assets/Scripts/VRtest.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VRtest : MonoBehaviour
 {
+    public int x = 0;
+    public Text Mytxt;
+    public GameObject click;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUptade()
+    {
+        x++;
+        Mytxt.text = x.ToString();
+    }
+    public void ClickBut()
+    {
+       click.SetActive(!(click.activeSelf));
     }
 }
